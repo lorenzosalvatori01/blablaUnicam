c31c492bdd921122742d5117aaa689d4f35b6a38
lorenzosalvatori01 <lorenzosalvatori01@email.com>
2024-02-22 19:31:48 +0100

    primo commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { HeaderComponent } from './header/header.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Test1';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
